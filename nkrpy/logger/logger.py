3d466cf374e44ab0aa7cbc4f2d0b96554d8c137d
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2019-12-30 00:29:31 -0600

    logger working

--- nkrpy/logger/logger.py
+++ nkrpy/logger/logger.py
@@ -1,22 +1,42 @@
 # internal modules
-from os import remove
-from os.path import isfile
-from glob import glob
-import datetime
-import time
+from datetime import datetime as datetime__datetime
+from time import sleep as time__sleep
+from time import time as time__time
+from atexit import register as atexit__register
+from signal import signal as signal__signal
+from signal import SIGTERM as signal__SIGTERM
+from sys import exit as sys__exit
 
 # external modules
 
 # relative modules
-from . import colours
+from .. import colours
 
-__all__ = ('Logger', )
+
+# global parameters
+datetime__today = datetime__datetime.today
+__all__ = (
+    "Logger",
+    "decorator",
+    "waiting",
+    "pyinput",
+    "debug",
+    "message",
+    "header1",
+    "header2",
+    "success",
+    "failure",
+    "warn",
+    "enable_colour",
+    "disable_colour",
+    "setup",
+)
 __doc__ = """."""
 __filename__ = __file__.split('/')[-1].strip('.py')
 __path__ = __file__.strip('.py').strip(__filename__)
 
 
-# function that creates a logger
+'''# function that creates a logger
 class SingletonMetaClass(type):
     def __init__(cls, name, bases, dict):
         super(SingletonMetaClass, cls).__init__(name, bases, dict)
@@ -28,111 +48,161 @@ class SingletonMetaClass(type):
             return cls.instance
 
         cls.instance = None
-        cls.__new__ = staticmethod(my_new)
+        cls.__new__ = staticmethod(my_new)'''
 
 
 class Logger(object):
-    """
-    The Messenger class which handles pretty
-    logging both to terminal and to a log
-    file which was intended for running 
-    codes on clusters in batch jobs where
+    """Generalized Logger.
+
+    Handles pretty logging both to terminal and to a log
+    file which was intended for running codes on clusters in batch jobs where
     terminals were not slaved.
     """
 
-    use_structure = ".    "
     __instance = None
-    __metaclass__ = SingletonMetaClass
+    __setup_status = False
 
-    def __init__(self, verbosity: int = 2, use_colour: bool = True,
-                 use_structure: bool = False, add_timestamp: bool = True,
-                 logfile=None):
+    def __new__(cls):
+        if cls.__instance is None:
+            instance = object.__new__(cls)
+            cls.__instance = instance
+        return cls.__instance
+
+    def __init__(self):
+        """Doesn't actually build the instance.
+        Need to separately call the __setup."""
+        pass
+
+    def __guarantee_setup(function):
+        """A specific wrapper to handle if
+        the overall class was setup properly."""
+        def wrapper(*args, **kwargs):
+            if not args[0].__setup_status:
+                args[0].setup()
+                args[0].warn("The logger command wasn't properly setup," +
+                             "fallback values were used.")
+            result = function(*args, **kwargs)
+            return result
+        return wrapper
+
+    def setup(self, append: bool = False,
+                verbosity: int = 2, use_colour: bool = True,
+                structure_string: str = ".    ", add_timestamp: bool = True,
+                logfile: str = None):
         """Set the parameters for the Messenger class.
+
+        Parameters
+        ----------
+        append: bool
+            If False, will write to new files based off of timestamp.
+            If True, will override the current files w/o timestamp
+        verbosity: int
+            Set the verbosity 1-5, 5 being higher
+        use_colour: bool
+            Turn on colour outputs
+        structure_string: str
+            Use a specified structure for logging.
+                will multiply this string by verbosity level to
+                determine left justification
+        add_timestamp: bool
+            Add a timestamp to the messages
+        logfile: str
+            This will serve as the base name for
+                the logfile. If unset will just
+                output to the terminal.
         """
-        self.verbosity = verbosity
+        saved_args = locals()
+        self.__setup_status = True
+        self.__dict__.update(saved_args)
+        self.set_verbosity(verbosity)
 
         # specifying colour options
-        self.use_colour = use_colour
         if use_colour:
             self.enable_colour()
         else:
             self.disable_colour()
 
-        self.use_structure = use_structure
-        self.add_timestamp = add_timestamp
-        self.logfile = logfile
-
-        # overrides existing file
-        if logfile is not None:
-            self.f = open(logfile, 'w')
-
-    def set_verbosity(self, verbosity):
-        """
-        Set the verbosity level for the class
-        """
-        self.verbosity = verbosity
-
-    def get_verbosity(self):
-        """
-        Returns the verbosity level of the class
-        """
-        return self.verbosity
-
-    def disable_colour(self):
-        """
-        Turns off all colour formatting.
-        """
-        for c in (self.BOLD, self.HEADER1, self.HEADER2,
-                  self.OKGREEN, self.WARNING, self.FAIL,
-                  self.MESSAGE, self.DEBUG, self.CLEAR):
-            c = ''
-
-    def enable_colour(self):
-        """
-        Enable all colour formatting.
-        """
-        self.__dict__ = {**colours.__dict__, **self.__dict__}
+        ctime = str(time__time()).split('.')[0]
+        self.write_status = 'a' if self.append else 'w'
+        if not self.append and self.logfile is not None:
+            self.logs = {'basename': self.logfile + f'-{ctime}'}
+        else:
+            self.logs = {'basename': self.logfile}
+        self.logfile = ''
 
     def _get_structure_string(self, level):
-        """
-        Returns the string of the message with the specified level
+        """Returns the string of the message with the specified level
         Which is dependent on the verbosity
         """
 
-        string = ''
-        if self.use_structure:
-            for i in range(level):
-                string = string + self.structure_string
+        string = self.structure_string * level
         return string
 
     def _get_time_string(self):
-        """
-        Returns the detailed datetime for extreme debugging
+        """Returns the detailed datetime for extreme debugging
         """
 
         string = ''
         if self.add_timestamp:
-            string = '[{}] '.format(datetime.datetime.today())
+            string = '[{}] '.format(datetime__today())
         return string
 
-    def _make_full_msg(self, msg, verb_level):
-        """
-        Constructs the full string that carries the message
-        with the specified verbosity parameters
+    def _make_full_msg(self, msg: str, verb_level: int):
+        """Construct the full string that carries the message.
+
+        With the specified verbosity parameters.
         """
         struct_string = self._get_structure_string(verb_level)
         time_string = self._get_time_string()
         return time_string + struct_string + msg
 
-    def _write(self, cmod: str, msg: str, out: bool = True):
-        """
-        Write the message to the file and print 
-        it to the terminal if it is wanted
+    def __set_logfile_base(self, *, basename: str):
+        """Set the basename for the logfile."""
+        self.logs['basename'] = basename
+
+    def __teardown(self):
+        """Close all opened files."""
+        if not hasattr(self, 'logs'):
+            return
+        for key in self.logs:
+            if key == 'basename':
+                continue
+            if not self.logs[key].closed:
+                self.logs[key].close()
+
+    def __sigHandler(self):
+        """Handle Ctl-C or sigints."""
+        self.__teardown()
+        sys__exit(0)
+
+    def _write(self, cmod: str, msg: str, out: bool = True, fname: str = None):
+        """Wrapper to write to terminal and file.
+
+        Write the message to the file and print
+        it to the terminal if it is wanted.
         """
         if out:
-            print("{}{}{}".format(cmod, msg, self.CLEAR))
-        if type(self.logfile) is str:
-            self.f.write(msg + '\n')
+            print("{}{}{}".format(cmod, msg, self._RST_))
+
+        if self.logs['basename'] is None:
+            return
+        if fname is not None:
+            self.logfile = '-'.join(self.logs['basename'], fname) + '.log'
+        elif self.logfile == '':
+            self.logfile = '-'.join(self.logs['basename'], '-1') + '.log'
+
+        if self.logfile in self.logs:
+            if self.logs[self.logfile].closed:
+                self.logs[self.logfile] = open(self.logfile,
+                                               self.__write_status)
+        else:
+            self.logs[self.logfile] = open(self.logfile, self.__write_status)
+        self.logs[self.logfile].write(msg + '\n')
+
+    def __resolve_style(self, style: str):
+        if hasattr(self, style):
+            return getattr(self, style)
+        return getattr(self, 'message')
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     """
@@ -141,85 +211,132 @@ class Logger(object):
     will handle writing to the log
     file and to the terminal
     """
+
+    def decorator(self, style: str = 'warn', verbosity: int = 2):
+        def real_decorator(function):
+            def wrapper(*args, **kwargs):
+                msg = f'Calling <{function.__name}> with ' +\
+                    f'params: {args}, {kwargs}'
+                (self.__resolve_style(style))(msg, verbosity)
+                result = function(*args, **kwargs)
+                (self.__resolve_style(style))(msg, verbosity)
+                msg = f'Called <{function.__name}> with ' +\
+                    f'params: {args}, {kwargs}. Result: {result}'
+                return result
+            return wrapper
+        return real_decorator
+
+    @__guarantee_setup
+    def set_verbosity(self, verbosity):
+        """Set the verbosity level for the class.
+        """
+        self.verbosity = verbosity
+
+    @__guarantee_setup
+    def get_verbosity(self):
+        """Return the verbosity level of the class.
+        """
+        return self.verbosity
+
+    @__guarantee_setup
+    def disable_colour(self):
+        """Turn off all colour formatting.
+        """
+        for c in colours.__dict__:
+            setattr(self, c, '')
+
+    @__guarantee_setup
+    def enable_colour(self):
+        """Enable all colour formatting.
+        """
+        self.__dict__ = {**colours.__dict__, **self.__dict__}
+
+    @__guarantee_setup
     def warn(self, msg, verb_level=2):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
             self._write(self.WARNING, full_msg)
 
+    @__guarantee_setup
     def header1(self, msg, verb_level=0):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
-            self._write(self.HEADER1, full_msg)
+            self._write(self.HEADER, full_msg)
 
+    @__guarantee_setup
     def header2(self, msg, verb_level=1):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
-            self._write(self.HEADER2, full_msg)
+            self._write(self.Cyan, full_msg)
 
+    @__guarantee_setup
     def success(self, msg, verb_level=1):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
             self._write(self.OKGREEN, full_msg)
 
+    @__guarantee_setup
     def failure(self, msg, verb_level=0):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
             self._write(self.FAIL, full_msg)
 
+    @__guarantee_setup
     def message(self, msg, verb_level=2):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
-            self._write(self.MESSAGE, full_msg)
+            self._write(self.OKBLUE, full_msg)
 
+    @__guarantee_setup
     def debug(self, msg, verb_level: int = 4):
 
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(msg, verb_level)
-            self._write(self.DEBUG, full_msg)
+            self._write(self.Yellow, full_msg)
 
+    @__guarantee_setup
     def pyinput(self, message: str = '', verb_level: int = 0):
 
         total_Message = "Please input {}: ".format(message)
         out = input(total_Message)
         if verb_level <= self.verbosity:
             full_msg = self._make_full_msg(total_Message, verb_level)
-            self._write(self.DEBUG, full_msg, False)
+            self._write(self.Yellow, full_msg, False)
         return out
 
+    @__guarantee_setup
     def waiting(self, auto: bool, seconds: int = 10, verb_level: int = 0):
         if not auto:
             self.pyinput('[RET] to continue or CTRL+C to escape')
         elif verb_level <= self.verbosity:
             self.warn('Will continue in {}s. CTRL+C to escape'.format(seconds))
-            time.sleep(seconds)
+            time__sleep(seconds)
 
-    def _REMOVE_(self, file: str):
-        """
-        This is a restructure of the os.system(rm) or the os.remove command
-        such that the files removed are displayed appropriately or not removed
-        if the file is not found
-        """
-        if type(file) is str:
-            for f in glob('*'+file+'*'):
-                if isfile(f):
-                    try:
-                        remove(f)
-                        self.debug("Removed file {}".format(f))
-                    except OSError:
-                        self.debug("Cannot find {} to remove".format(f))
-        else:
-            for f in file:
-                if isfile(f):
-                    try:
-                        remove(f)
-                        self.debug("Removed file {}".format(f))
-                    except OSError:
-                        self.debug("Cannot find {} to remove".format(f))
+    @__guarantee_setup
+    def teardown(self):
+        self.__teardown()
+
+    @__guarantee_setup
+    def sigHandler(self):
+        self.__sigHandler()
+
+
+# Yielding singleton to module
+_logger = Logger()
+
+for func in __all__:
+    if func == 'Logger':
+        continue
+    globals()[func] = getattr(_logger, func)
+
+# handling exit
+atexit__register(_logger.teardown)
+signal__signal(signal__SIGTERM, _logger.sigHandler)
 
 # end of code
