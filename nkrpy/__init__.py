9a9b7fb7ff618ecac863b32ae7469632d1c9232c
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2020-01-03 12:53:02 -0600

    changing to require setup build

--- nkrpy/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dustmodels import kappa, kappa_models, kappascale
-from . import constants
-from . import math
-from .unit import unit, astroconversion
-from . import publication
-
-__all__ = ('kappa', 'kappa_models', 'kappascale',
-           'constants', 'math', 'unit', 'publication',
-           'astroconversion')
---
+++ nkrpy/__init_generator__.py
@@ -0,0 +1,18 @@
+"""DESC"""
+from .astro import dustmodels
+from . import math
+from .unit import Unit, convert
+from . import publication
+from .misc import (colours, constants, decorators, errors, mp,
+                   typecheck, addspace, between,
+                   find_nearest, strip, get, list_comp,
+                   add, find_max)
+
+__all__ = ('dustmodels',
+           'math',
+           'Unit', 'convert',
+           'publication',
+           'colours', 'constants', 'decorators', 'errors', 'mp',
+           'typecheck', 'addspace', 'between',
+           'find_nearest', 'strip', 'get', 'list_comp',
+           'add', 'find_max')
